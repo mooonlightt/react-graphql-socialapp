bced1af7f4600dbbb2f63e6fdb033ebbd90c9f26
mooonlightt <mooonlightt44@gmail.com>
2021-03-27 15:10:15 -0300

    make it better

---
+++ config.js
@@ -0,0 +1,12 @@
+// module.exports = {
+//   MONGODB:
+//     'mongodb+srv://classsed:6lxxxlexaTslHLPe@cluster0-pcsru.mongodb.net/merng?retryWrites=true',
+//   SECRET_KEY: 'some very secret key'
+// };
+
+
+module.exports ={
+  MONGODB : 'mongodb+srv://TNReactApollo01:TNReact123@cluster0.unjwn.mongodb.net/merng?retryWrites=true&w=majority',
+
+  SECRET_KEY : 'secret key'
+}
